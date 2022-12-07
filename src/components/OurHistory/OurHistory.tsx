47de7b973a75301ce8c7ff65dede180bb2d1178d
Fosa1990 <fosa1990@gmail.com>
2022-12-07 22:04:43 +0200

    fix OurHistory

--- src/components/OurHistory/OurHistory.tsx
+++ src/components/OurHistory/OurHistory.tsx
@@ -12,7 +12,7 @@ const OurHistory: FC = () => {
     <Section
       color="#fff"
       background="#000"
-      backgroundImage={ourHistoryBackground}
+      backgroundImage={`url(${ourHistoryBackground})`}
       padding={{
         topMob: '40px',
         bottomMob: '40px',
